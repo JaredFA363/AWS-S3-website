c9aff97b9a874cff2aeb8a665d0d739bf24e95d8
Jared <jaredawiti2017@gmail.com>
2024-12-28 13:09:04 +0000

    Added public access to bucket, changed control ownership and added ACL

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,30 @@
 resource "aws_s3_bucket" "websitebucket" {
   bucket = var.bucketname
 }
+
+resource "aws_s3_bucket_ownership_controls" "websitebucket" {
+  bucket = aws_s3_bucket.websitebucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "websitebucket" {
+  bucket = aws_s3_bucket.websitebucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "websitebucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.websitebucket,
+    aws_s3_bucket_public_access_block.websitebucket,
+  ]
+
+  bucket = aws_s3_bucket.websitebucket.id
+  acl    = "public-read"
+}
